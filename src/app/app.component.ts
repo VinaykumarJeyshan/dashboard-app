5f17c5d92e45884eebb906b886a39c10ec3f564e
Vinay <vinay@administrator-Latitude-E5470>
2019-07-12 20:44:09 +0530

    made template changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,10 @@ import { HelloWorld } from '../components/hello-world/hello-world.component';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  event: any =
-    {
-      time: "08:00",
-      subject: "Breakfast with Simon",
-      location: "Lounge Caffe",
-      description: "Discuss Q3 targets"
-    };
+  events: Array<any> = [
+    {time: '08:00', subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
+    {time: '08:30', subject: 'Daily Standup Meeting (recurring)', location: 'Warsaw Spire Office'},
+    {time: '09:00', subject: 'Call with HRs'},
+    {time: '12:00', subject: 'Lunch with Timmoty', location: 'Canteen', description: 'Project evalutation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a'},
+  ];
 }
